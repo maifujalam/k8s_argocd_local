4d20612fd1c73d8d82b4205c51de539cabd381dd
alam <sk.maifujalam@gmail.com>
2023-10-21 12:26:21 +0530

    argocd on k8s with vagrant

---
+++ README.md
@@ -0,0 +1,3 @@
+# K8s Argocd Local
+Create local kubernetes setup using vagrant and manage all apps with argocd.
+
---
+++ ShellScripts/list_applications.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ARGOCD_SERVER=argocd.master.com
+ARGOCD_SERVER="localhost:8080"
+USERNAME=admin
+PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+# echo $PASSWORD
+
+argocd login $ARGOCD_SERVER  --insecure  --username $USERNAME --password $PASSWORD  # --grpc-web  --skip-test-tls
+
+printf "\nList of ArgoCD Apps\n"
+argocd app list
+printf "\nList of ArgoCD Projects\n"
+argocd proj list
